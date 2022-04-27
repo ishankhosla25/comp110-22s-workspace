e86131398188436abf788500849d6ece7a20dd3b
Ishan Khosla <ishank@ad.unc.ede>
2022-04-27 00:56:42 -0400

    Finished EX07

--- exercises/ex07/data_utils.py
+++ exercises/ex07/data_utils.py
@@ -34,15 +34,6 @@ def column_values(table: list[dict[str, str]], column: str) -> list[str]:
     return result
 
 
-def columnar(row_table: list[dict[str, str]]) -> dict[str, list[str]]:
-    """Transform a row-oriented table to a column-oriented table."""
-    result: dict[str, list[str]] = {}
-    first_row: dict[str, str] = row_table[0]
-    for column in first_row:
-        result[column] = column_values(row_table, column)
-    return result
-
-
 def head(rand_table: dict[str, list[str]], N: int) -> dict[str, list[str]]:
     """Create a table with heading and first few rows."""
     result: dict[str, list[str]] = {}
@@ -53,4 +44,46 @@ def head(rand_table: dict[str, list[str]], N: int) -> dict[str, list[str]]:
             row_values.append(rand_table[key][i])
             i += 1
         result[key] = row_values
+    return result
+
+
+def select(rand_table: dict[str, list[str]], column_names: list[str]) -> dict[str, list[str]]:
+    """Proudce a column based table with only specific columns."""
+    result: dict[str, list[str]] = {}
+    for name in column_names:
+        result[name] = rand_table[name]
+    return result
+
+
+def concat(table_1: dict[str, list[str]], table_2: dict[str, list[str]]) -> dict[str, list[str]]:
+    """Cobine 2 tables."""
+    result: dict[str, list[str]] = {}
+    for key in table_1:
+        result[key] = table_1[key]
+    for key in table_2:
+        if key in result:
+            result[key] += table_2[key]
+        else:
+            result[key] = table_2[key]
+    return result
+
+
+def count(x: list[str]) -> dict[str, int]:
+    result: dict[str, int] = {}
+    i: int = 0
+    while i < len(x):
+        if x[i] in result:
+            result[x[i]] += 1
+        else:
+            result[x[i]] = 1
+        i += 1
+    return result
+
+
+def columnar(row_table: list[dict[str, str]]) -> dict[str, list[str]]:
+    """Transform a row-oriented table to a column-oriented table."""
+    result: dict[str, list[str]] = {}
+    first_row: dict[str, str] = row_table[0]
+    for column in first_row:
+        result[column] = column_values(row_table, column)
     return result
