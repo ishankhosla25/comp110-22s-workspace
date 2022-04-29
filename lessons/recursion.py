b58ab9d718d8420328febf385dd3abd3993e2500
Ishan Khosla <ishank@ad.unc.ede>
2022-04-28 23:52:11 -0400

    Recursion

--- lessons/recursion.py
+++ lessons/recursion.py
@@ -1,18 +1,43 @@
 from __future__ import annotations
 
-from typing import Union
+from typing import Optional
 
 
 class Node:
     data: int
-    next: Union[Node, None]
+    next: Optional[Node]
 
-    def __init__(self, data: int, next: Union[Node, None]):
+    def __init__(self, data: int, next: Optional[Node]):
         """Constructor."""
         self.data = data
         self.next = next
 
+    def __str__(self) -> str:
+        """Creates a str."""
+        if self.next is None:
+            return f"{self.data} -> None"
+        else:
+            return f"{self.data} -> {self.next}"
 
-head: Node = Node(1, None)
-tail: Node = Node(2, None)
-head.next = tail
+
+def sum(node: Optional[Node]) -> int:
+    if node is None:
+        return 0
+    else:
+        return node.data + sum(node.next)
+
+
+def count(node: Optional[Node], current_count: int = 0) -> int:
+    """Return the number of nodes in a list."""
+    if node is None:
+        return current_count
+    else:
+        return count(node.next, current_count + 1)
+
+
+head: Node = Node(3, None)
+head = Node(2, head)
+head = Node(1, head)
+print(sum(head))
+print(count(head))
+print(head)
